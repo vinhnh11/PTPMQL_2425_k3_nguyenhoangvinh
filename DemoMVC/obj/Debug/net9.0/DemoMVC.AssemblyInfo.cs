f645f2251fa45d4de4af5f4ab5cf17e0b590e30d
Vinh Nguyen <nhv28112004@gmail.com>
2025-06-27 00:47:31 +0700

    HTML

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a303e5a3ffaa58d533e824931df3b4ed8860cb33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb34ba0cec97542eb0e80e7bfd5ba1a0926e7e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
